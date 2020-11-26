22fc7229c62c4ed8b0ac467d7e32fc5434750798
Michael Stucki <mipictrost@gmail.com>
2020-11-26 14:26:38 +0100

    first aframe test

---
+++ iterations/iteration1/app.js
@@ -0,0 +1,27 @@
+class App {
+    constructor() {
+
+    }
+
+    async init() {
+
+    }
+
+    async onEnterAR() {
+
+    }
+
+    onNoXRDevice() {
+
+    }
+
+    async onSessionStarted(session) {
+
+    }
+
+    onXRFrame(time, frame) {
+
+    }
+};
+
+window.app = new App();
